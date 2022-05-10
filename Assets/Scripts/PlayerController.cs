f8f204e34855ef693733789fbaaba6ffa4ff6579
poronite <72969946+poronite@users.noreply.github.com>
2022-05-10 12:55:25 +0100

    Camera movement with mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,33 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//depois coloco comentarios
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float speed = 2.0f;
 
+    [SerializeField]
+    private float mouseSensitivity = 15f;
+
+    [SerializeField]
+    private float verticalViewClamp = 80f;
+
     private CharacterController player;
     private InputManager inputManager;
+    private Camera mainCamera;
+    private Transform cameraPivot;
+    private float horizontalView;
+    private float verticalView;
+
     private Vector2 inputVector;
     private Vector3 movement;
-    
+    private float cameraPivotRotationX;
 
     private void Start()
     {
         player = GetComponent<CharacterController>();
         inputManager = InputManager.InputInstance;
+        mainCamera = Camera.main;
+        cameraPivot = GameObject.FindGameObjectWithTag("CameraPivot").transform;
     }
 
     void Update()
     {
         inputVector = inputManager.GetPlayerMovement();
-        movement = new Vector3(inputVector.x, 0, inputVector.y);
+        movement = mainCamera.transform.forward.normalized * inputVector.y + mainCamera.transform.right.normalized * inputVector.x;
 
         player.Move(movement * Time.deltaTime * speed);
 
-        if (movement != Vector3.zero)
-        {
-            gameObject.transform.forward = movement;
-        }
+        Vector3 position = transform.position;
+        position.y = 1f;
+        transform.position = position;
+
+        horizontalView = inputManager.GetPlayerLook().x * mouseSensitivity * Time.deltaTime;
+        verticalView = inputManager.GetPlayerLook().y * mouseSensitivity * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log($"Horizontal View = {horizontalView} | Vertical View = {verticalView}");
+
+        cameraPivotRotationX -= verticalView;
+        cameraPivotRotationX = Mathf.Clamp(cameraPivotRotationX, -verticalViewClamp, verticalViewClamp);
+        cameraPivot.localRotation = Quaternion.Euler(cameraPivotRotationX, 0f, 0f);
+
+        transform.Rotate(Vector3.up * horizontalView);
     }
 }
