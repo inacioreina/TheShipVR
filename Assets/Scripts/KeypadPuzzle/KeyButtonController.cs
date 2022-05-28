c29ed860442c77c949b8b6c81c0acb55db1e0e3f
poronite <72969946+poronite@users.noreply.github.com>
2022-05-28 12:44:00 +0100

    Created a script to handle keypad's button presses

---
+++ Assets/Scripts/KeypadPuzzle/KeyButtonController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyButtonController : MonoBehaviour
+{
+    private string keyButtonID;
+
+    delegate void InputCode(string key);
+    InputCode KeyButtonPress;
+
+
+
+    private void Start()
+    {
+        SetKeyButtonID();
+
+        SetDelegates();
+    }
+
+
+    private void SetDelegates()
+    {
+        KeypadController keypadController = transform.parent.GetComponent<KeypadController>();
+
+        KeyButtonPress = keypadController.GetKeyPress;
+
+        Debug.Log($"{gameObject.name} connected to {keypadController.name}");
+    }
+
+
+    private void SetKeyButtonID()
+    {
+        keyButtonID = gameObject.name.Replace("Key", string.Empty);
+
+        Debug.Log($"{gameObject.name} ID: {keyButtonID}");
+    }
+
+
+    //when player presses the key
+    public void ButtonPress()
+    {
+        KeyButtonPress.Invoke(keyButtonID);
+    }
+}
